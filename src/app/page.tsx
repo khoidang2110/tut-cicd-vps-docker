0cc00407aae0c6f4046afa828b78fefdb3370710
khoidang2110 <khoidang2110@gmail.com>
2024-08-09 16:40:23 +0700

    img2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  <p>github action test img 1</p>
+  <p>github action test img 2</p>
     </main>
   );
 }
