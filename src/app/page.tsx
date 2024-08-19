9075936a1b97f3306404ea6b293b01e9fd7bfcd8
khoidang2110 <khoidang2110@gmail.com>
2024-08-19 09:44:23 +0700

    clone1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  <p>github action test img 3</p>
+  <p>github action - using clone</p>
     </main>
   );
 }
