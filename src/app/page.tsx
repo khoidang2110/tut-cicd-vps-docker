243fdc32f4610e73b343aeebb0ecc1a4a0b89b62
khoidang2110 <khoidang2110@gmail.com>
2024-09-05 14:29:48 +0700

    false 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
- //console.log(a)
+ console.log(a)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
   <p>github action - up fisrt 2</p>
