e1e6d07f7d948633be6cdba9c61f88bb8ba9180d
khoidang2110 <khoidang2110@gmail.com>
2024-09-05 14:18:40 +0700

    fisrt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
  //console.log(a)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  <p>github action - remove log a</p>
+  <p>github action - up fisrt</p>
     </main>
   );
 }
